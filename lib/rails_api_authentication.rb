6fabc153b4ec2e68030a915b8e44b17315defd8c
liyijie <liyijie825@gmail.com>
2017-04-30 21:50:35 +0800

    Add require files to module file.

--- lib/rails_api_authentication.rb
+++ lib/rails_api_authentication.rb
@@ -1,5 +1,9 @@
 require "rails_api_authentication/version"
+require "rails_api_authentication/auth_action"
+require "rails_api_authentication/auth_session"
+require "rails_api_authentication/auth_token"
+require "rails_api_authentication/authable"
 
 module RailsApiAuthentication
-  
+
 end
