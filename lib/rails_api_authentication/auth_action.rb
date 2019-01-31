f52f7261fd8e3ef163ff9b7387d8f4540ddcdc26
liyijie <liyijie825@gmail.com>
2019-01-31 23:13:15 +0800

    instance_variable_set when authentication is done

--- lib/rails_api_authentication/auth_action.rb
+++ lib/rails_api_authentication/auth_action.rb
@@ -10,6 +10,8 @@ module RailsApiAuthentication
       klasses = self.class.action_classes
       klasses.each do |klass|
         @current_auth = klass.auth!(request) rescue next
+        sym = klass.name.underscore.to_sym
+        instance_variable_set("@current_#{sym}", @current_auth)
         break
       end
       render(json: "Unauthorized", status: 401) if @current_auth.nil?
@@ -22,7 +24,6 @@ module RailsApiAuthentication
 
         @klasses = Array(klass_sym).map do |sym|
           define_method("current_#{sym}") { @current_auth }
-          instance_variable_set("@current_#{sym}", @current_auth)
           sym.to_s.camelize.constantize
         end
       end
